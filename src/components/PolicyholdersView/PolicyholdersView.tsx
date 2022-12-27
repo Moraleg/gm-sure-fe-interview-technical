3178a0ecd72c59b5601de182cbaab338bb4c99e7
Geraldine Morales <geraldinemorales@Geraldines-MBP.attlocal.net>
2022-12-27 01:07:22 -0500

    feat: render get request results

--- src/components/PolicyholdersView/PolicyholdersView.tsx
+++ src/components/PolicyholdersView/PolicyholdersView.tsx
@@ -1,24 +1,60 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import InfoTable from '../InfoTable';
 import { Box } from '@mui/material';
 import  axios  from 'axios'
 
-async function getPolicyholders() {
-  try {
-    const response = await axios.get('https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders');
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
+interface Policyholder {
+  name: string;
+  age: number;
+  address: {
+    line1: string;
+    line2: string;
+    city: string;
+    state: string;
+    postalCode: string;
+  };
+  phoneNumber: string;
+  isPrimary: boolean;
 }
 
 function PolicyholdersView() {
+  const [ policyHolders, setPolicyHolders ] = useState<Array<Policyholder>>([]);
+
+  const getPolicyholders = async () => {
+      await axios.get('https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders')
+      .then((response) => {
+        const { policyHolders } = response?.data || {}
+        setPolicyHolders(policyHolders)
+      
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+   
+  }
+
   useEffect(() => {
-    getPolicyholders()
+   getPolicyholders()
   }, []);
+
+  const policyHoldersModified =  Array(5)
+  .fill(null)
+  .map((_, i) => Object.assign({}, ...policyHolders));
+  console.log(policyHolders)
+
+
+  const rows = policyHoldersModified && policyHoldersModified.map((item) => ({
+    name: `NAME: ${item?.name}`,
+    age: `AGE: ${item?.age}`,
+    address: `ADDRESS: ${item?.address?.line1} ${item?.address?.line2} ${item?.address?.city}, ${item?.address?.state} ${item?.address?.postalCode}`,
+    phoneNumber: `PHONE NUMBER: ${item?.phoneNumber}`,
+    primary: `Primary Policyholder: ${item?.isPrimary}`
+  }))
+
 
   return (
     <Box sx={{ textAlign: 'center' }}>
-      {'POLICYHOLDERS VIEW'}
+      <InfoTable header="Policyholder tabs" rows={rows} />
     </Box>
   );
 }
